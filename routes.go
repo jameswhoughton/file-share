8debe8ee5074afafd75779db9965a778b39683ca
James Houghton <james@jwhoughton.co.uk>
2024-08-09 19:28:45 +0100

    update templates

--- routes.go
+++ routes.go
@@ -8,7 +8,7 @@ import (
 
 func getLoginHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFS(templateFiles, "templates/layout.gohtml", "templates/login.gohtml")
+		tmpl, err := template.ParseFS(templateFiles, "templates/layout.gohtml", "templates/form.gohtml", "templates/login.gohtml")
 
 		if err != nil {
 			w.Write([]byte("Template error: " + err.Error()))
